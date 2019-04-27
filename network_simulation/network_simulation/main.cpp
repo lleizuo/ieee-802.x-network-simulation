a846abcf71157006b1d631499b8c8a60d8037679
Lei Zuo <lzuo98@gmail.com>
2019-04-26 20:33:47 -0700

    add the cpp project

---
+++ network_simulation/network_simulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  network_simulation
+//
+//  Created by 左磊 on 2019/4/26.
+//  Copyright © 2019 ECS 152A. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
